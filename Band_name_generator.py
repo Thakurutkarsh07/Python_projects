6a0017ce6e846fd8069cc7a060ed9d4bbae8db7a
Utkarsh Thakur <thakurutkarsh.0700@gmail.com>
2023-02-28 13:05:39 +0530

    Update Band_name_generator.py

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,6 +1,5 @@
-print("Enter your country's name?")
-C_name = input()
-print("Enter your pet's name?")
-P_name = input()
 
+print("Welcome to band name generator!")
+C_name = input("Enter your country's name?")
+P_name = input("Enter your pet's name?")
 print(C_name +" "+ P_name)
